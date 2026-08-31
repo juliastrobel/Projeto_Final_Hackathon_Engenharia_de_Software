2105077962c26184410816c9a719ad28781f5f5a
julia <juliapstrobel786@gmail.com>
2026-08-31 10:03:36 -0300

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE teams (
+    id INTEGER PRIMARY KEY,
+    team_name TEXT,
+    leader_email TEXT,
+    leader_email_verified BOOLEAN DEFAULT 0,
+    verify_token TEXT,
+    repo_full_name TEXT
+);
+
+CREATE TABLE team_members (
+    id INTEGER PRIMARY KEY,
+    team_id INTEGER REFERENCES teams(id),
+    member_name TEXT
+);
