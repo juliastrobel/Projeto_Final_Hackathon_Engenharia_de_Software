514299a24301cc876d2e346dd94352912d0b7b18
julia <juliapstrobel786@gmail.com>
2026-08-31 19:29:16 -0300

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
-CREATE TABLE IF NOT EXISTS teams (
+CREATE TABLE teams (
     id INTEGER PRIMARY KEY,
     team_name TEXT,
+    leader_name TEXT,
     leader_email TEXT,
     leader_email_verified BOOLEAN DEFAULT 0,
     verify_token TEXT,
@@ -8,7 +9,7 @@ CREATE TABLE IF NOT EXISTS teams (
     github_username TEXT
 );
 
-CREATE TABLE IF NOT EXISTS team_members (
+CREATE TABLE team_members (
     id INTEGER PRIMARY KEY,
     team_id INTEGER REFERENCES teams(id),
     member_name TEXT
